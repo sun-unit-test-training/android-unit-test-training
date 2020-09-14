d0e24f99a785affa8743d1d11f3989f9405ac94b
BacNV <nguyen.van.bac@sun-asterisk.com>
2020-09-14 17:07:42 +0700

    Home screen

---
+++ app/src/main/java/com.sun.training.ut/ui/home/HomeActivity.kt
@@ -0,0 +1,46 @@
+package com.sun.training.ut.ui.home
+
+import android.content.Intent
+import android.os.Bundle
+import com.sun.training.ut.BR
+import com.sun.training.ut.R
+import com.sun.training.ut.data.model.Exercise
+import com.sun.training.ut.databinding.ActivityHomeBinding
+import com.sun.training.ut.ui.base.BaseActivity
+import org.koin.android.viewmodel.ext.android.viewModel
+
+/**
+ * Sample activity
+ * Show list post got from URL_END_POINT = https://jsonplaceholder.typicode.com/
+ */
+class HomeActivity : BaseActivity<ActivityHomeBinding, HomeViewModel>(), HomeItemClickListener {
+    override val viewModel: HomeViewModel by viewModel()
+    override val bindingVariable = BR.viewModel
+    override val layoutId = R.layout.activity_home
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setUpAdapter()
+    }
+
+    private fun setUpAdapter() {
+        viewBinding.adapter = HomeAdapter(this).apply {
+            submitList(listOf(
+                Exercise("Ex1: Bia hơi Keangnam", this@HomeActivity.javaClass),
+                Exercise("Ex2: ATM của ngân hàng Việt Nam", this@HomeActivity.javaClass),
+                Exercise("Ex3: Quần áo nam trên Hoàn Kiếm", this@HomeActivity.javaClass),
+                Exercise("Ex4: Mr.A đang làm 1 app về calendar", this@HomeActivity.javaClass),
+                Exercise("Ex5: 8 pieces pizza", this@HomeActivity.javaClass),
+                Exercise("Ex6: Trung tâm mua sắm Tây Hồ", this@HomeActivity.javaClass),
+                Exercise("Ex7: Vietnam Mart", this@HomeActivity.javaClass),
+                Exercise("Ex8: Mr. A đến một sân chơi nọ để chơi cầu lông", this@HomeActivity.javaClass),
+                Exercise("Ex9: Roll Playng Game Hanoi quest", this@HomeActivity.javaClass),
+                Exercise("Ex10: Nhà hàng Nam Từ Liêm", this@HomeActivity.javaClass)
+            ))
+        }
+    }
+
+    override fun onHomeItemClicked(item: Exercise<*>) {
+        startActivity(Intent(this, item.clz))
+    }
+}
